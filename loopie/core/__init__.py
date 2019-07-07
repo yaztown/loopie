aa1cc8d7c37d66a65a7902f06b2caa3f99c42384
Yaztown <yaztown@gmail.com>
2019-07-07 19:25:49 +0300

    working

--- loopie/core/__init__.py
+++ loopie/core/__init__.py
@@ -3,5 +3,6 @@
 from .application import Application
 from .main_loop import MainLoop
 from .base_threads.base_threads import LoopingThread
+from .settings import load_settings_from_file
 
-__all__ = ['Application', 'MainLoop', 'LoopingThread']
+__all__ = ['Application', 'MainLoop', 'LoopingThread', 'load_settings_from_file']
