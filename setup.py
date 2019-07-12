1db7e5f7f2513cc45fb833609c9ca0e33645ea8a
Yaztown <yaztown@gmail.com>
2019-07-12 10:52:31 +0300

    Some modification to the setup.py file
    - Added some required classifiers to setup.py
    - Removed the '.txt' from the LICENSE filename

--- setup.py
+++ setup.py
@@ -1,12 +1,56 @@
 from setuptools import setup
 
-setup(name='loopie',
-		version='0.0.1',
-		url='http://yaztown.com/loopie',
-		author='Yaztown',
-		author_email='yaztown@gmail.com',
+setup(
+		name='loopie',
+		packages=['loopie'],
+		version='0.1',
+		license="GPLv3",
 		description='A simple looper framework',
 		long_description='A simple looper framework for python3 with multiple features',
-		licence='MIT',
-		packages=['loopie'],
-		zip_safe=False)
+		author='Yaztown',
+		author_email='yaztown@gmail.com',
+		url='https://github.com/yaztown/loopie',
+		download_url = 'https://github.com/yaztown/loopie/archive/v_0_1.tar.gz',
+		keywords = ['loopie', 'looping', 'mainloop', 'thread', 'threading', 'Pi', 'Raspberry Pi', 'RPi'],
+		classifiers=[
+		    'Development Status :: 3 - Alpha',
+		    'Intended Audience :: Developers',
+		    'Topic :: Software Development :: Build Tools',
+		    'License :: OSI Approved :: GPLv3 License',
+		    'Programming Language :: Python :: 3',
+		    'Programming Language :: Python :: 3.4',
+		    'Programming Language :: Python :: 3.5',
+		    'Programming Language :: Python :: 3.6',
+		    'Programming Language :: Python :: 3.7',
+		 ],
+)
+
+'''
+from distutils.core import setup
+setup(
+  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
+  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license = 'MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'YOUR NAME',                   # Type in your name
+  author_email = 'your.email@domain.com',      # Type in your E-Mail
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
+'''
