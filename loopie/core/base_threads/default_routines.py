27f3093c905fd25d74645ae52c92c2d289ef32bf
Yaztown <yaztown@gmail.com>
2019-07-12 19:51:47 +0300

    Removed the print statement

--- loopie/core/base_threads/default_routines.py
+++ loopie/core/base_threads/default_routines.py
@@ -6,11 +6,24 @@ Created on Monday 27/05/2019
 from .base_exceptions import ThreadExitException
 import signal
 
+from loopie.logging import get_logger
+logger = get_logger()
+
+
 def shutdown_routine(signum, frame):
     sig = signal._int_to_enum(signum,signal.Signals)
-    print('\nReceived {}.'.format(sig.name))
+    logger.debug('\n\nReceived {}.\n'.format(sig.name))
     raise ThreadExitException()
 
 def register_exit_signal_handler():
     signal.signal(signal.SIGTERM, shutdown_routine)
     signal.signal(signal.SIGINT, shutdown_routine)
+
+'''
+    If you are using eclipse or any other IDE to debug your app, then you
+    can use the below command to search and kill your. Just change <appname>
+    to your app's name.
+        
+        kill -SIGINT $(ps aux | grep <appname> | grep -v grep| awk '{print $2}')
+
+'''
