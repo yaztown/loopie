e96daf5fd4a8f7295b520616d268bfeb0d015338
Yaztown <yaztown@gmail.com>
2019-07-13 07:43:41 +0300

    Updated for v0.2

--- setup.py
+++ setup.py
@@ -1,25 +1,42 @@
+'''
+Created on Saturday 13/07/2019
+
+@author: yaztown
+'''
+
 from setuptools import setup
 
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
 setup(
-		name = 'loopie',
-		packages = ['loopie'],
-		version = '0.1',
-		license = "GPLv3",
-		description = 'A simple looper framework',
-		long_description = 'A simple looper framework for python3 with multiple features',
-		author = 'Yaztown',
-		author_email = 'yaztown@gmail.com',
-		url = 'https://github.com/yaztown/loopie',
-		download_url = 'https://github.com/yaztown/loopie/archive/v0.1.tar.gz',
-		keywords = ['loopie', 'looping', 'mainloop', 'thread', 'threading', 'Pi', 'Raspberry Pi', 'RPi'],
-		install_requires = [
+		name='loopie',
+		packages=['loopie'],
+		version='0.2',
+		license="GPLv3",
+		description='A simple python framework for creating a looping application (Service).',
+		long_description=README,
+		long_description_content_type="text/markdown",
+# 		python_requires='~=3.6',
+		author='Yaztown',
+		author_email='yaztown@gmail.com',
+		url='https://github.com/yaztown/loopie',
+		download_url='https://github.com/yaztown/loopie/archive/v0.2.tar.gz',
+		keywords=['loopie', 'loop', 'looping', 'mainloop', 'thread', 'threading', 'Pi', 'Raspberry Pi', 'RPi'],
+		install_requires=[
 	        'flask',
 	        'flask_cors',
 	        'flask_jsonrpc',
 	        'wsgiserver',
 	        'pytz',
       	],
-		classifiers = [
+		classifiers=[
 		    'Development Status :: 5 - Production/Stable',
 		    'Intended Audience :: Developers',
 		    'Topic :: Software Development :: Build Tools',
